dc39efbc29577b80253669c794e8e81a28fff09c
Damiano Chintala <damianochintala@gmail.com>
2020-03-26 09:52:05 +0200

    Updates

---
+++ precache-manifest.3beac3023ca930534aa226f753752619.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df86ac493a0556f27b9dfdcbcfd1198c",
+    "url": "/sizmat/index.html"
+  },
+  {
+    "revision": "111301ede284caed9b79",
+    "url": "/sizmat/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "569b1c516f82f531a1ed",
+    "url": "/sizmat/static/css/main.980c81eb.chunk.css"
+  },
+  {
+    "revision": "111301ede284caed9b79",
+    "url": "/sizmat/static/js/2.43a4c6e1.chunk.js"
+  },
+  {
+    "revision": "6e71b6ee5ea7775d33431df95d229f09",
+    "url": "/sizmat/static/js/2.43a4c6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "569b1c516f82f531a1ed",
+    "url": "/sizmat/static/js/main.f3e3817a.chunk.js"
+  },
+  {
+    "revision": "423d5b0b47c9d45b59c4",
+    "url": "/sizmat/static/js/runtime-main.f351d334.js"
+  },
+  {
+    "revision": "1016eb73ef6069571202b28a2a727b73",
+    "url": "/sizmat/static/media/back1.1016eb73.jpg"
+  },
+  {
+    "revision": "5b654988b85931ccee5b9f2a17b60f3f",
+    "url": "/sizmat/static/media/back2.5b654988.jpg"
+  },
+  {
+    "revision": "217ccab31f4494f88ab1cac6fadd3084",
+    "url": "/sizmat/static/media/car1.217ccab3.jpg"
+  },
+  {
+    "revision": "5c5db67b58ae4e697d307c1ca98d04e9",
+    "url": "/sizmat/static/media/car2.5c5db67b.jpg"
+  },
+  {
+    "revision": "241f9fdf1fcf8bba8da90f5d77af08f5",
+    "url": "/sizmat/static/media/car3.241f9fdf.jpg"
+  },
+  {
+    "revision": "e8453ab2824def789b094e03f0099d01",
+    "url": "/sizmat/static/media/car4.e8453ab2.jpg"
+  },
+  {
+    "revision": "e742df014d54dffd80b765f0972e5d4b",
+    "url": "/sizmat/static/media/car5.e742df01.jpg"
+  },
+  {
+    "revision": "766aec818789e2f585b12b911acbb21f",
+    "url": "/sizmat/static/media/car6.766aec81.jpg"
+  },
+  {
+    "revision": "334f4ddceeda3a4706fa75c6975bc326",
+    "url": "/sizmat/static/media/car7.334f4ddc.jpg"
+  },
+  {
+    "revision": "1ef21992a3efd81b66f3ae57b9124e34",
+    "url": "/sizmat/static/media/logo.1ef21992.jpg"
+  },
+  {
+    "revision": "03117b29c3d5d581494728b98b245dcd",
+    "url": "/sizmat/static/media/us1.03117b29.jpg"
+  },
+  {
+    "revision": "bd68b88b00e068e8c565ea5273ce2c08",
+    "url": "/sizmat/static/media/us2.bd68b88b.jpg"
+  },
+  {
+    "revision": "5cc7758da171352ca209c9db190f4d9b",
+    "url": "/sizmat/static/media/us3.5cc7758d.jpg"
+  }
+]);
